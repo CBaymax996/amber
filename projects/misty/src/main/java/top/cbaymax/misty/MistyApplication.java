7cfbc7c913ee5b577c23efb3b7e0359690f50e0d
chuhao <cbaymax@qq.com>
2024-07-19 00:12:49 +0800

    init misty

---
+++ projects/misty/src/main/java/top/cbaymax/misty/MistyApplication.java
@@ -0,0 +1,14 @@
+package top.cbaymax.misty;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MistyApplication.class, args);
+    }
+
+}
